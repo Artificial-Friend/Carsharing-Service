41f940ef71fbcc87cb76469064d6baf9ffb35327
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-01-13 11:17:42 +0200

    refactored according to comments

--- src/main/java/core/basesyntax/service/implementations/CarServiceImpl.java
+++ src/main/java/core/basesyntax/service/implementations/CarServiceImpl.java
@@ -1,72 +1,64 @@
 package core.basesyntax.service.implementations;
 
 import core.basesyntax.dao.DaoCar;
+import core.basesyntax.dao.DaoDriver;
 import core.basesyntax.lib.Inject;
 import core.basesyntax.lib.Service;
 import core.basesyntax.model.Car;
 import core.basesyntax.model.Driver;
 import core.basesyntax.service.CarService;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class CarServiceImpl implements CarService {
     @Inject
-    private DaoCar dao;
+    private DaoCar daoCar;
+    @Inject
+    private DaoDriver daoDriver;
 
     @Override
     public Car create(Car item) {
-        return dao.create(item);
+        return daoCar.create(item);
     }
 
     @Override
     public Car get(Long id) {
-        return dao.get(id).get();
+        return daoCar.get(id).get();
     }
 
     @Override
     public List<Car> getAll() {
-        return dao.getAll();
+        return daoCar.getAll();
     }
 
     @Override
     public Car update(Car item) {
-        return dao.update(item);
+        return daoCar.update(item);
     }
 
     @Override
     public boolean delete(Long id) {
-        return dao.delete(id);
+        return daoCar.delete(id);
     }
 
     @Override
     public void addDriverToCar(Driver driver, Car car) {
-        List<Driver> drivers = car.getDrivers();
-        drivers.add(driver);
-        car.setDrivers(drivers);
-        dao.update(car);
+        car.getDrivers().add(driver);
+        daoCar.update(car);
     }
 
     @Override
     public void removeDriverFromCar(Driver driver, Car car) {
-        if (car.getDrivers().contains(driver)) {
-            List<Driver> drivers = car.getDrivers();
-            drivers.remove(driver);
-            car.setDrivers(drivers);
-            dao.update(car);
-        } else {
-            System.out.println("ERROR: driver " + driver + " doesn't exist for car " + car);
-        }
+        car.getDrivers().remove(driver);
+        daoCar.update(car);
     }
 
     @Override
     public List<Car> getAllByDriver(Long driverId) {
-        return getAll()
-                .stream()
-                .filter(car -> car.getDrivers()
-                        .stream()
-                        .map(Driver::getId)
-                        .collect(Collectors.toList()).contains(driverId))
-                .collect(Collectors.toList());
+        Optional<Driver> driver = daoDriver.get(driverId);
+        List<Car> cars = daoCar.getAll();
+        cars.removeIf(car -> !car.getDrivers().contains(driver));
+        return cars;
     }
 }
